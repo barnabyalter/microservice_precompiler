b359ae3a8bda94d5a1d28d5247c48ead796f8d78
B Alter <barnaby.alter@gmail.com>
2015-04-29 13:11:08 -0400

    Updated gems in gemspec to latest :gem: and updated tests

--- microservice_precompiler.gemspec
+++ microservice_precompiler.gemspec
@@ -8,24 +8,19 @@ Gem::Specification.new do |gem|
   gem.summary       = %q{The microservice precompiler uses a handful of technologies to compile Javascripts and Stylesheets and create HTML pages from templates into a distribution folder ready for deployment.}
   gem.homepage      = "https://github.com/barnabyalter/microservice-precompiler"
 
-  
+
   gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = "microservice_precompiler"
   gem.require_paths = ["lib"]
   gem.version       = MicroservicePrecompiler::VERSION
-  
-  #gem.add_development_dependency "rspec", "~> 2.6"
-  gem.add_dependency "rake", "~> 10.0.2"
-  gem.add_development_dependency "debugger"
-  gem.add_development_dependency "travis-lint"
-  gem.add_development_dependency "reek"
-  gem.add_dependency "compass", "~> 0.12.1"
-  gem.add_dependency "sprockets", "~> 2.8.2"
-  gem.add_dependency "uglifier", "~> 1.3.0"
-  gem.add_dependency "mustache", "~> 0.99.4"
-  gem.add_dependency "yui-compressor", "~> 0.9.6"
-  gem.add_dependency "coffee-script", "~> 2.2.0"
-end
 
+  gem.add_dependency "rake", "~> 10.0.2"
+  gem.add_dependency "compass", "~> 1.0.0"
+  gem.add_dependency "sprockets", "~> 3.0.0"
+  gem.add_dependency "uglifier", "~> 2.7.1"
+  gem.add_dependency "mustache", "~> 1.0.0"
+  gem.add_dependency "yui-compressor", "~> 0.12.0"
+  gem.add_dependency "coffee-script", "~> 2.4.0"
+end
--- test/unit/builder_test.rb
+++ test/unit/builder_test.rb
@@ -1,15 +1,14 @@
 require 'rubygems'
-require 'ruby-debug'
 require 'test_helper'
 
 class BuilderTest < Test::Unit::TestCase
-  
+
   def setup
     @project_root = File.dirname(__FILE__) + '/../dummy'
     @precompiler = MicroservicePrecompiler::Builder.new(@project_root)
     @sprocket_assets = [:javascripts, :stylesheets]
   end
-  
+
   def test_initialize
     precompiler = nil
     assert_nothing_raised(){ precompiler = MicroservicePrecompiler::Builder.new(@project_root) }
@@ -17,10 +16,10 @@ class BuilderTest < Test::Unit::TestCase
     assert_equal(precompiler.project_root, @project_root)
     assert_not_nil(precompiler.build_path)
     assert_equal(precompiler.build_path, @project_root + "/dist")
-    assert_not_nil(precompiler.mustaches_config)
-    assert_equal(precompiler.mustaches_config, "mustaches.yml.tml")
+    assert_not_nil(precompiler.mustaches_filename)
+    assert_equal(precompiler.mustaches_filename, "mustaches.yml.tml")
   end
-  
+
   def test_cleanup
     cleanup = nil
     assert_nothing_raised(){ cleanup = @precompiler.cleanup }
@@ -30,13 +29,13 @@ class BuilderTest < Test::Unit::TestCase
     assert((File.exists? "#{@project_root}/dist/javascripts"), "No javascripts folder found")
     assert((File.exists? "#{@project_root}/dist/templates"), "No templates folder found")
   end
-  
+
   def test_compass_build
     compass = nil
     assert_nothing_raised(){ compass = @precompiler.compass_build }
     assert((File.exists? "#{@project_root}/stylesheets"), "No stylesheets folder found, compass build failed")
   end
-  
+
   def test_sprockets_build
     sprockets = nil
     assert_nothing_raised(){ sprockets = @precompiler.sprockets_build }
@@ -48,7 +47,7 @@ class BuilderTest < Test::Unit::TestCase
     #Check that the files in javascripts are minimized
     #TODO
   end
-  
+
   def test_mustache_build
     mustache = nil
     assert_nothing_raised(){ mustache = @precompiler.mustache_build }
@@ -56,6 +55,6 @@ class BuilderTest < Test::Unit::TestCase
     assert((File.exists? "#{@project_root}/dist/templates"), "No templates folder found")
     assert((File.exists? "#{@project_root}/dist/templates/Sample.html"), "File not found.")
   end
-  
+
 
 end
